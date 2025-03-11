03c64b2b40346b8ef98762900c208b47694acc07
animapace <123429557+animapace@users.noreply.github.com>
2025-03-11 19:34:41 -0400

    Create herenciasimple.cs

---
+++ herenciasimple.cs
@@ -0,0 +1,11 @@
+class Animal {
+    public void HacerSonido() {
+        Console.WriteLine("Hace un sonido");
+    }
+}
+
+class Perro : Animal {
+    public void Ladrar() {
+        Console.WriteLine("Guau Guau");
+    }
+}
